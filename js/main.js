09a537de3366a62faf83c4e8555d37c7d32019f9
André Vital <santiago_vital@outlook.com>
2020-06-01 18:08:17 -0500

    Added js files

---
+++ js/main.js
@@ -0,0 +1,184 @@
+(function($) {
+	"use strict";
+
+  	/*---------------------------------------------------- */
+  	/* FitText Settings
+  	------------------------------------------------------ */
+  	setTimeout(function(){
+   		$('#intro h1').fitText(1,{minFontSize:'42px', maxFontSize:'84px'});
+  	}, 100);
+
+	/*----------------------------------------------------- */
+	/* Alert Boxes
+  	------------------------------------------------------- */
+	$('.alert-box').on('click', '.close', function(){
+		$(this).parent().fadeOut(500);
+	});
+
+
+	/*---------------------------------------------------- */
+	/*	Masonry
+	------------------------------------------------------ */
+	var containerProjects = $('#folio-wrapper');
+
+	containerProjects.imagesLoaded(function(){
+		containerProjects.masonry({
+		  	itemSelector: '.folio-item',
+		  	resize: true
+		});
+	});
+
+
+	/*----------------------------------------------------*/
+	/*	Modal Popup
+	------------------------------------------------------*/
+	$('.item-wrap a').magnificPopup({
+		type:'inline',
+		fixedContentPos: false,
+		removalDelay: 300,
+		showCloseBtn: false,
+		mainClass: 'mfp-fade'
+	});
+
+	$(document).on('click', '.popup-modal-dismiss', function(e){
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+
+	/*-----------------------------------------------------*/
+  	/* Navigation Menu
+   ------------------------------------------------------- */
+	var toggleButton = $('.menu-toggle'),
+		nav = $('.main-navigation');
+
+	// toggle button
+	toggleButton.on('click', function(e){
+		e.preventDefault();
+		toggleButton.toggleClass('is-clicked');
+		nav.slideToggle();
+	});
+
+	// nav items
+  	nav.find('li a').on("click", function(){
+		toggleButton.toggleClass('is-clicked');
+		nav.fadeOut();
+  	});
+
+
+    /*---------------------------------------------------- */
+  	/* Scroll Spy
+  	------------------------------------------------------ */
+	var sections = $("section"),
+	navigation_links = $("#main-nav-wrap li a");
+
+	sections.waypoint({
+		handler: function(direction){
+			var active_section;
+			active_section = $('section#' + this.element.id);
+
+			if (direction === "up") active_section = active_section.prev();
+
+			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+			navigation_links.parent().removeClass("current");
+			active_link.parent().addClass("current");
+		}, offset: '25%'
+	});
+
+	/*---------------------------------------------------- */
+  	/* Light Gallery
+  	------------------------------------------------------ */
+
+	$('#live_out_15-gl').lightGallery({
+    	thumbnail:true,
+	    animateThumb: false,
+	    showThumbByDefault: false
+	});
+
+	$('#live_out_16-gl').lightGallery({
+    	thumbnail:true,
+	    animateThumb: false,
+	    showThumbByDefault: false,
+	    youtubePlayerParams: {
+	        modestbranding: 1,
+	        showinfo: 0,
+	        rel: 0,
+	        controls: 2
+	    }
+	});
+
+	/*---------------------------------------------------- */
+  	/* Smooth Scrolling
+  	------------------------------------------------------ */
+  	$('.smoothscroll').on('click', function(e){
+	 	e.preventDefault();
+
+   		var target = this.hash,
+    	$target = $(target);
+
+    	$('html, body').stop().animate({
+       		'scrollTop': $target.offset().top
+      	}, 800, 'swing', function(){
+	      	window.location.hash = target;
+	    });
+
+  	});
+
+	/*---------------------------------------------------- */
+	/*	contact form
+	------------------------------------------------------ */
+
+	/* local validation */
+	$('#contactForm').validate({
+		submitHandler: function(form) {
+			var sLoader = $('#submit-loader');
+
+			$.ajax({
+				type: "POST",
+				url: "sendEmail.php",
+				data: $(form).serialize(),
+				beforeSend: function() {
+					sLoader.fadeIn();
+				},
+
+				success: function(msg) {
+					if (msg == 'OK') {
+						sLoader.fadeOut();
+						$('#message-warning').hide();
+						$('#contactForm').fadeOut();
+						$('#message-success').fadeIn();
+					} else {
+						sLoader.fadeOut();
+						$('#message-warning').html(msg);
+						$('#message-warning').fadeIn();
+					}
+				},
+
+				error: function(msg) {
+					sLoader.fadeOut();
+					$('#message-warning').html(msg);
+					$('#message-warning').fadeIn();
+				}
+			});
+		}
+	});
+
+ 	/*----------------------------------------------------- */
+  	/* Back to top
+   ------------------------------------------------------- */
+	var pxShow = 300; // height on which the button will show
+	var fadeInTime = 400; // how slow/fast you want the button to show
+	var fadeOutTime = 400; // how slow/fast you want the button to hide
+	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+
+   // Show or hide the sticky footer button
+	jQuery(window).scroll(function() {
+		if (!( $("#header-search").hasClass('is-visible'))) {
+			if (jQuery(window).scrollTop() >= pxShow) {
+				jQuery("#go-top").fadeIn(fadeInTime);
+			} else {
+				jQuery("#go-top").fadeOut(fadeOutTime);
+			}
+		}
+	});
+})(jQuery);
